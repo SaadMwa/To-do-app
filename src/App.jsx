94a91f575ad6dbfacb972c0114dc7906bfcb3b6f
Saad <mwasaad47@gmail.com>
2025-08-29 11:08:45 -0700

    Fixed App.jsx imports with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
+// App.jsx
+
+import React from 'react'
 import './App.css'
+
+// Components + their CSS
 import Navbar from './components/Navbar'
-import './components/navbar.css'
+import './components/Navbar.css'
+
 import Feature from './components/Feature'
 import './components/Feature.css'
-import Search from './components/Search'
-import './components/Search.css'
+
 import Arrival from './components/Arrival'
 import './components/Arrival.css'
+
 import Plants from './components/Plants'
 import './components/Plants.css'
+
+import Search from './components/Search'
+import './components/Search.css'
+
 import Last from './components/Last'
 import './components/Last.css'
 
-
-
-
-
 function App() {
-
   return (
-    <>
-      <div>
-        <Navbar />
-       <Feature />
-       <Search />
-       <Arrival />
-       <Plants />
-       <Last/>
-
-       
-       
-      </div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Feature />
+      <Arrival />
+      <Plants />
+      <Search />
+      <Last />
+    </div>
   )
 }
 
