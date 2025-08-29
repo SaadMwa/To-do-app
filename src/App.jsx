424c138052d5badb1543494e05b81e692ddfcb5e
Saad <mwasaad47@gmail.com>
2025-08-29 10:51:20 -0700

    Fix imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import './components/navbar.css'
-import './components/Feature.css'
 import Feature from './components/Feature'
+import './components/Feature.css'
 import Search from './components/Search'
 import './components/Search.css'
 import Arrival from './components/Arrival'
 import './components/Arrival.css'
-import  Plants from './components/Plants'
+import Plants from './components/Plants'
 import './components/Plants.css'
-import './components/Last.css'
 import Last from './components/Last'
+import './components/Last.css'
 
 
 
